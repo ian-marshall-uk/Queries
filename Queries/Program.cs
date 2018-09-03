198225c8372fa662b15510cccf40cf90e93eea71
Ian @ home <ian@visdata.co.uk>
2018-09-03 23:12:34 +0100

    Section 6, lecture 53

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Data.Entity.Core;
 using System.Linq;
 
@@ -11,7 +10,7 @@ namespace Queries
         {
             var context = new PlutoContext();
 
-            LinqSyntax7(context);
+            DeferredExecution(context);
 
         }
 
@@ -155,9 +154,9 @@ namespace Queries
 
         }
 
-        static void ExtensionMethods(PlutoContext context)
+        static void ExtensionMethods1(PlutoContext context)
         {
-            Console.WriteLine("EntensionMethods");
+            Console.WriteLine("EntensionMethods1");
             Console.WriteLine();
 
             var courses = context.Courses
@@ -169,5 +168,172 @@ namespace Queries
                 Console.WriteLine(course.Name);
             }
         }
+
+        static void ExtensionMethods2(PlutoContext context)
+        {
+            Console.WriteLine("EntensionMethods2");
+            Console.WriteLine();
+
+            var courses = context.Courses
+                .Where(c => c.Level == 1);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+
+        static void ExtensionMethods3(PlutoContext context)
+        {
+            Console.WriteLine("EntensionMethods3");
+            Console.WriteLine();
+
+            var courses = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenBy(c => c.Author);
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+
+        static void ExtensionMethods4(PlutoContext context) // Projection
+        {
+            Console.WriteLine("EntensionMethods4");
+            Console.WriteLine();
+
+            var courses = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenByDescending(c => c.Author)
+                .Select(c => new
+                {
+                    CourseName = c.Name,
+                    AuthorName = c.Author.Name
+                });
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.CourseName);
+            }
+        }
+
+        static void ExtensionMethods4a(PlutoContext context) // Projection 2
+        {
+            Console.WriteLine("EntensionMethods4a");
+            Console.WriteLine();
+
+            var tags = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                .ThenByDescending(c => c.Author)
+                .SelectMany(c => c.Tags)
+                .Distinct();
+
+            foreach (var tag in tags)
+            {
+                Console.WriteLine(tag.Name);
+            }
+        }
+
+        static void ExtensionMethods5(PlutoContext context) // Groups
+        {
+            Console.WriteLine("EntensionMethods5");
+            Console.WriteLine();
+
+            var groups = context.Courses.GroupBy(c => c.Level);
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine("Key: {0}", group.Key);
+                foreach (var course in group)
+                {
+                    Console.WriteLine("\t{0}", course.Name);
+                }
+            }
+        }
+
+        static void ExtensionMethods6(PlutoContext context) // Groups
+        {
+            Console.WriteLine("EntensionMethods6");
+            Console.WriteLine();
+
+            var courses = context.Courses
+                .Join(context.Authors,
+                    c => c.AuthorId,
+                    a => a.Id, (course, author) => new
+                    {
+                        CourseName = course.Name,
+                        AuthorName = author.Name
+                    });
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine("{0}. {1}", course.CourseName, course.AuthorName);
+            }
+        }
+
+        static void ExtensionMethods7(PlutoContext context) // Groups
+        {
+            Console.WriteLine("EntensionMethods7");
+            Console.WriteLine();
+
+            var courses = context.Authors.GroupJoin(context.Courses, a => a.Id, c => c.AuthorId, (author, course) => new
+            {
+                Author = author,
+                CourseCount = course.Count()
+            });
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine("{0}, {1}", course.Author.Name, course.CourseCount);
+            }
+        }
+
+        static void ExtensionMethods8(PlutoContext context) // Pagination
+        {
+            Console.WriteLine("EntensionMethods8");
+            Console.WriteLine();
+
+            var courses = context.Courses.OrderBy(c => c.Name).Skip(2).Take(2);
+            foreach (var course in courses)
+            {
+                Console.WriteLine("{0}, {1}", course.Author.Name, course.Name);
+            }
+        }
+
+        static void ExtensionMethods9(PlutoContext context) // Elements
+        {
+            Console.WriteLine("EntensionMethods9");
+            Console.WriteLine();
+
+            var course = context.Courses.OrderBy(c => c.Level).FirstOrDefault(c => c.FullPrice > 25);
+            Console.WriteLine("{0}, {1}", course.Author.Name, course.Name);
+
+            var AllAbove10Quid = context.Courses.All(c => c.FullPrice > 50);
+            var AnyCoursesInLevel1 = context.Courses.Any(c => c.Level == 1);
+            Console.WriteLine("{0}, {1}", AllAbove10Quid, AnyCoursesInLevel1);
+
+            var count = context.Courses.Count();
+            var mostExpensive = context.Courses.Max(c => c.FullPrice);
+
+            var count2 = context.Courses.Where(c => c.Level == 1).Count();
+            var count3 = context.Courses.Count(c => c.Level == 1);
+
+            Console.WriteLine("{0}, {1}, {2}. {3}", count, mostExpensive, count2, count3);
+
+        }
+
+        static void DeferredExecution(PlutoContext context) // Elements
+        {
+            var courses = context.Courses;
+            foreach (var course in courses)
+            {
+                Console.WriteLine(course.Name);
+            }
+        }
+
     }
 }
